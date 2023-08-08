3762d42ed44009114e46c797d6793ea03739afb9
Richard Garcia <103012321+richardgarciajr@users.noreply.github.com>
2023-08-08 18:18:58 -0500

    Update resources_valid.tf

--- resources_valid.tf
+++ resources_valid.tf
@@ -1,52 +1,52 @@
-#resource "random_string" "random_name" {
-#  length  = 10
-#  special = false
-#  upper   = false
-#}
+resource "random_string" "random_name" {
+  length  = 10
+  special = false
+  upper   = false
+}
 
 
-#resource "aws_s3_bucket" "opa_bucket" {
-#    bucket        = format("opa-encryption-%s", random_string.random_name.result)
-#   acl           = "private"
-#   force_destroy = true
-#     tags = {
-#     department = "Learning"
-#     owner      = "ASE"
-#   }
-# }
-
-
-
-# resource "aws_subnet" "public-subnet" {
-#   cidr_block        = var.public_subnet_cidr
-#   vpc_id            = aws_vpc.stack-example-vpc.id
-#   availability_zone = "us-west-2a"
-
-#   tags = {
-#     department = "Learning"
-#     owner      = "ASE"
-#   }
-# }
+resource "aws_s3_bucket" "opa_bucket" {
+    bucket        = format("opa-encryption-%s", random_string.random_name.result)
+   acl           = "private"
+   force_destroy = true
+     tags = {
+     department = "Learning"
+     owner      = "ASE"
+   }
+ }
 
 
 
-# resource "aws_internet_gateway" "gateway" {
-#   vpc_id = aws_vpc.stack-example-vpc.id
+ resource "aws_subnet" "public-subnet" {
+   cidr_block        = var.public_subnet_cidr
+   vpc_id            = aws_vpc.stack-example-vpc.id
+   availability_zone = "us-west-2a"
 
-#   tags = {
-#     department = "Learning"
-#     owner      = "ASE"
-#   }
+   tags = {
+     department = "Learning"
+     owner      = "ASE"
+   }
+ }
 
-# }
 
 
-# resource "aws_vpc" "stack-example-vpc" {
-#   cidr_block           = var.vpc_cidr
-#   enable_dns_support   = true
-#   enable_dns_hostnames = true
-#   tags = {
-#     department = "Learning"
-#     owner      = "ASE"
-#   }
-# }
+ resource "aws_internet_gateway" "gateway" {
+   vpc_id = aws_vpc.stack-example-vpc.id
+
+   tags = {
+     department = "Learning"
+     owner      = "ASE"
+   }
+
+ }
+
+
+ resource "aws_vpc" "stack-example-vpc" {
+   cidr_block           = var.vpc_cidr
+   enable_dns_support   = true
+   enable_dns_hostnames = true
+   tags = {
+     department = "Learning"
+     owner      = "ASE"
+   }
+ }
